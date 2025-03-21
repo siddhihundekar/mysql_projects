493c6cc7a4074d381d9debb1ab60b44f68a30723
Siddhi Pravin Hundekar <hundekarsiddhi@gmail.com>
2025-03-21 15:01:06 +0530

    Delete mysql_collegeManagement directory

--- mysql_collegeManagement/home.php
+++
@@ -1,7 +0,0 @@
-<?php include ("layouts/header.php"); ?>
-
-<?php include ("layouts/navbar.php"); ?>
-
-<h1 style="text-align:center;margin:1em;">Welcome to College Management</h1>
-
-<?php include ("layouts/footer.php"); ?>
